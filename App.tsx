b513532c19536270630a0b057e9ba2f1f7d6aa01
phr31 <48303421+phr31@users.noreply.github.com>
2022-07-20 17:26:22 -0300

    Sign In screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
-import { NativeBaseProvider } from "native-base";
-import { SignIn } from './src/screens/SignIn'
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { THEME } from "./src/styles/theme";
+import { SignIn } from './src/screens/SignIn';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { Loading } from "./src/components/Loading"
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   return (
-    <NativeBaseProvider>
-      <SignIn />
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+      {fontsLoaded ? <SignIn /> : <Loading/>}
     </NativeBaseProvider>
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,37 @@
-import { View, Text } from 'react-native';
-import { NativeBaseProvider } from "native-base";
+import { useState } from "react";
+import { VStack, Heading, Icon, useTheme } from "native-base";
+import Logo from "../assets/logo_primary.svg";
+import { Envelope, Key} from 'phosphor-react-native'
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 
 export function SignIn(){
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+  const { colors } = useTheme()
+  function handleSignIn() {
+    console.log(name, password);
+  }
+
   return (
-    <NativeBaseProvider>
-      <Text>Olá, Paulo.</Text>
-    </NativeBaseProvider>
+    <VStack flex={1} alignItems = "center" bg="gray.600" px={8} pt={24}>
+      <Logo/>
+      <Heading color="gray.100" fontSize='xl' mt={20} mb={6}>
+        Acesse sua conta {name}
+      </Heading>
+      <Input placeholder="E-mail" 
+             mb={4} 
+             InputLeftElement={<Icon as={<Envelope color={colors.gray[300]}/>} ml={4}/>}
+             onChangeText={setName}
+             />
+             
+      <Input placeholder="Senha"
+            mb={8}
+             InputLeftElement={<Icon as={<Key color={colors.gray[300]}/>} ml={4}/>}
+             secureTextEntry
+             onChangeText={setPassword}
+             />
+      <Button title="Entrar" w='full' onPress={handleSignIn}/>     
+    </VStack>
   )
 }
